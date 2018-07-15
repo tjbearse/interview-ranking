6f18cddccc1bf24a1bfcd9b19cc335bc85eaacfe
TJ Bearse <thomasb@qualtrics.com>
2018-07-14 18:03:37 -0700

    clean up and command line args for real.py

--- src/real.py
+++ src/real.py
@@ -1,38 +1,66 @@
+from optparse import OptionParser
 import itertools
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 import ratings
 
-labels = {}
-with open('real/labels.tsv', 'r') as f:
-    for l in f:
-        name, n = l.strip().split("\t")
-        labels[n] = name
+def main():
+    labels = getLabels()
 
-loops = []
-with open('real/anon.txt', 'r') as f:
-    l = f.readline()
-    loop = []
-    while l != "":
-        if l == "\n":
-            loops.append(loop)
-            loop = []
-        else:
-            i, v = l.strip().split('\t')
-            i = int(i)
-            v = v=="Yes"
-            loop.append((i,v))
+    loops = getLoops()
+    graph = ratings.buildGraph(loops)
+    ratings.weightGraphUntilConverge(graph, 20)
+
+    if options.graph:
+        ratings.graph2Digraph(graph, True, labels)
+
+
+    if options.plot:
+        nodes = np.array([(node.v, node.nI, len(node.e)) for _, node in graph.iteritems()],
+                dtype=[('ratings', 'f4'), ('interviews', 'i4'), ('comparisons', 'i4')]
+            )
+        plt.scatter(nodes['ratings'], nodes['comparisons'], s=nodes['interviews'])
+        plt.show()
+
+def getLabels():
+    labels = {}
+    with open(options.labels, 'r') as f:
+        for l in f:
+            name, n = l.strip().split("\t")
+            labels[n] = name
+    return labels
+
+def getLoops():
+    loops = []
+    with open(options.loops, 'r') as f:
         l = f.readline()
-# print "\n".join(map(str, loops))
-# print "\n".join(map(str, comparisons))
-graph = ratings.buildGraph(loops)
-ratings.weightGraphUntilConverge(graph, 20)
-# ratings.graph2Digraph(graph, True, labels)
+        loop = []
+        while l != "":
+            if l == "\n":
+                loops.append(loop)
+                loop = []
+            else:
+                i, v = l.strip().split('\t')
+                v = v=="Yes"
+                loop.append((i,v))
+            l = f.readline()
+    return loops
 
+if __name__ == "__main__":
+    labelFile = os.path.join(os.path.dirname(__file__), '..', "real", "labels.tsv")
+    loopFile = os.path.join(os.path.dirname(__file__), '..', "real", "anon.txt")
+    parser = OptionParser()
+    parser.add_option("-g", "--graph", action="store_true", dest="graph",
+                      help="graph relationships")
+    parser.add_option("-p", "--plot", action="store_true", dest="plot",
+                      help="show plots about relationships")
+    parser.add_option("--labels", dest="labels", default=labelFile,
+                      help="file to pull labels from")
+    parser.add_option("--loops", dest="loops", default=loopFile,
+                      help="file to pull loops from")
 
-nodes = np.array([(node.v, node.nI, len(node.e)) for _, node in graph.iteritems()],
-        dtype=[('ratings', 'f4'), ('interviews', 'i4'), ('comparisons', 'i4')]
-    )
-plt.scatter(nodes['ratings'], nodes['comparisons'], s=nodes['interviews'])
-plt.show()
+    (options, args) = parser.parse_args()
+
+    main()
